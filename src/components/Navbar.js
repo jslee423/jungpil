b93ed7df7b053fce74c4b6626f4db7f9d47345a2
Jason Lee <jslee423@gmail.com>
2023-02-11 12:52:26 -0500

    add project dropdown menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,31 @@
-import { Link, NavLink } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className='nav'>
         <div className='nav-brand'>
-            <Link to='/' className='site-title'>JUNGPIL®</Link>
+            <NavLink to='/' id='site-title'>JUNGPIL®</NavLink>
         </div>
         <div className='nav-links'>
             <ul>
                 <li>
-                    <NavLink to='/work'>WORK</NavLink>
+                    <div class="dropdown">
+                        <NavLink to='/work'>WORK</NavLink>
+                        <div class="dropdown-content">
+                            <NavLink to="/project1">Project Title</NavLink>
+                            <NavLink to="project2">Title of Project</NavLink>
+                            <NavLink to="project3">Project Title</NavLink>
+                            <NavLink to="project4">Title of Project</NavLink>
+                            <NavLink to="project5">Project Title</NavLink>
+                            <NavLink to="project6">Title of Project</NavLink>
+                            <NavLink to="project7">Project Title</NavLink>
+                            <NavLink to="project8">Title of Project</NavLink>
+                            <NavLink to="project9">Project Title</NavLink>
+                            <NavLink to="project10">Title of Project</NavLink>
+                            <NavLink to="project11">Project Title</NavLink>
+                        </div>
+                    </div>
                 </li>
                 <li>
                     <NavLink to='/about'>ABOUT</NavLink>
