f47cc528a7873c0b92f2a27096ff3bcf6c217f64
Jason Lee <jslee423@gmail.com>
2023-04-05 10:49:17 -0400

    update router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,34 @@
-import './App.css';
 import Navbar from './components/Navbar';
-import DropdownNav from './components/DropdownNav';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Work from './pages/Work';
-import About from './pages/About';
-import Footer from './components/Footer';
-import projects from './utils/projects';
 import Project from './pages/Project';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import DropdownNav from './components/DropdownNav';
+import { projects } from './utils/projects';
+import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-  const location = useLocation();
-  
-  return (
-    <div className="App">
-      <DropdownNav />
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/work" element={<Work/>} />
-        {projects.map((project, index) => {
-          return (
-            <Route path={`/work/${project.path}`} element={<Project />} key={index} />
-          )
-        })}
-        <Route path="/about" element={<About/>} />
-        <Route path="/shop" element={<Work/>} />
-        <Route path="/cart" element={<Work/>} />
-      </Routes>
-      {location.pathname === "/" ? "" : <Footer />}
-    </div>
-  );
+    // const location = useLocation();
+    
+    return (
+        <div className="App">
+            <DropdownNav />
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<Home/>} />
+                <Route path="/work" element={<Work/>} />
+                {projects.map((project, index) => {
+                    return (
+                        <Route path={`/work/${project.path}`} element={<Project name={project.name} />} key={index} />
+                    )
+                })}
+                <Route path="/about" element={<Work/>} />
+                <Route path="/shop" element={<Work/>} />
+                <Route path="/cart" element={<Work/>} />
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
