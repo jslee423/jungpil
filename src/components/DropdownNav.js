c7eceadee8bb8ee5b09ce4dee326e265bd5eaaa2
Jason Lee <jslee423@gmail.com>
2023-04-05 10:41:33 -0400

    disable for mobile and update scroll function

--- src/components/DropdownNav.js
+++ src/components/DropdownNav.js
@@ -4,19 +4,15 @@ import './DropdownNav.scss';
 const DropdownNav = () => {
     const location = useLocation();
     const currentPathArray = location.pathname.replace('/', '').split('/');
-    // console.log('loc', location);
-    function scrollFunction() {
-        // console.log(document.body.scrollTop, document.documentElement.scrollTop)
-        if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+    const scrollFunction = () => {
+        if (document.body.scrollTop > '200' || document.documentElement.scrollTop > '200') {
             document.getElementById("DropdownNav").style.top = '0';
         } else {
             document.getElementById("DropdownNav").style.top='-100px';
         }
     }
 
-    window.onscroll = function() {
-        scrollFunction();
-    }
+    window.addEventListener('scroll', scrollFunction);
 
     return (
         <div id='DropdownNav'>
@@ -27,7 +23,7 @@ const DropdownNav = () => {
                         return (
                             <div key ={index} className='nav-crumb'>
                                 <p>&#62;</p>
-                                <NavLink to={location.pathname} element="">{path.replaceAll('_', ' ').toUpperCase()}</NavLink>
+                                <NavLink to={location.pathname} element="">{path.replaceAll('_', ' ')}</NavLink>
                             </div>
                         )
                     } else {
