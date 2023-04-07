33f5bd0f9c564cbad4a9f038334478f4bd69e8a1
Jason Lee <jslee423@gmail.com>
2023-04-07 16:15:23 -0400

    add about page

---
+++ src/pages/About.js
@@ -0,0 +1,12 @@
+import './About.scss';
+
+const About = () => {
+    return (
+        <div className="About">
+            <div className='about-sec-1'></div>
+            <div className='about-sec-2'></div>
+        </div>
+    );
+};
+
+export default About;
