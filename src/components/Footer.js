cfb6a5718a1ee79d6b8c9784d85222ed50abb832
Jason Lee <jslee423@gmail.com>
2023-04-07 16:11:57 -0400

    add footer

---
+++ src/components/Footer.js
@@ -0,0 +1,23 @@
+import squid from '../img/squid.png';
+import { NavLink } from 'react-router-dom';
+import './Footer.scss';
+
+const Footer = () => {
+    return (
+        <div className='Footer'>
+            <div className='footer-links'>
+                <NavLink to='/'><img src={squid} alt='squid' id='squid' /></NavLink>
+                <NavLink to='/contact' className="foot-link">CONTACT</NavLink>
+                <NavLink to='/client' className="foot-link">CLIENT</NavLink>
+                <NavLink to='/search' className="foot-link">SEARCH</NavLink>
+            </div>
+            <p id='copyright'>Copyright JUNGPIL® 2022 • Don’t steal my shit.</p>
+            <div className='small-copyright'>
+                <p>Copyright JUNGPIL® 2022</p>
+                <p>Don’t steal my shit.</p>
+            </div>
+        </div>
+    )
+};
+
+export default Footer;
