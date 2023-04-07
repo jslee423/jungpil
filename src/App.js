0958ccc4281b90ad9f98e4d9ffca8d909ce094da
Jason Lee <jslee423@gmail.com>
2023-04-07 16:13:22 -0400

    update about route and add footer

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import Navbar from './components/Navbar';
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
 import Home from './pages/Home';
 import Work from './pages/Work';
 import Project from './pages/Project';
 import Shop from './pages/Shop';
 import DropdownNav from './components/DropdownNav';
+import Footer from './components/Footer';
+import About from './pages/About';
 import { projects } from './utils/projects';
 import './App.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
-    // const location = useLocation();
+    const location = useLocation();
     
     return (
         <div className="App">
@@ -24,10 +26,11 @@ function App() {
                         <Route path={`/work/${project.path}`} element={<Project name={project.name} />} key={index} />
                     )
                 })}
-                <Route path="/about" element={<Work/>} />
+                <Route path="/about" element={<About/>} />
                 <Route path="/shop" element={<Shop/>} />
                 <Route path="/cart" element={<Work/>} />
             </Routes>
+            {location.pathname === '/' ? "" : <Footer />}
         </div>
     );
 }
