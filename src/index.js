bb92ae7984ec9f6d6d3e0325f6d3073ee352c3ee
Jason Lee <jslee423@gmail.com>
2023-05-26 14:02:12 -0400

    fix dependency issues

--- src/index.js
+++ src/index.js
@@ -1,32 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthUserProvider } from './firebase/auth';
 
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//     <AuthContextProvider>
-//         <React.StrictMode>
-//             <BrowserRouter>
-//                 <App />
-//             </BrowserRouter>
-//         </React.StrictMode>
-//     </AuthContextProvider>
-// );
-
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
     <AuthUserProvider>
         <React.StrictMode>
             <BrowserRouter>
                 <App />
             </BrowserRouter>
         </React.StrictMode>
-    </AuthUserProvider>,
-    document.getElementById('root')
-  );
+    </AuthUserProvider>
+);
+
+// ReactDOM.render(
+//     <AuthUserProvider>
+//         <React.StrictMode>
+//             <BrowserRouter>
+//                 <App />
+//             </BrowserRouter>
+//         </React.StrictMode>
+//     </AuthUserProvider>,
+//     document.getElementById('root')
+//   );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
